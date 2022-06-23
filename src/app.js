73215d9a666e07a2b1638dbf0d758a939d1e2126
FelipeV1993 <f.vasquezossa@gmail.com>
2022-06-23 00:24:46 +0000

    excuse gitpod

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function azar(x) {
+    let numero = Math.floor(Math.random() * x.length);
+    return numero;
+  }
+  var text = `${who[azar(who)]} ${action[azar(action)]} ${what[azar(what)]} ${
+    when[azar(when)]
+  } `;
+  document.querySelector("#excuse").innerHTML = text;
 };
